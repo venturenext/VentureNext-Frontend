ba316347ca7bd12127c2251b5b313cd41031c4bf
Moh Raihan Romadhoni <dhoni@volantis.io>
2025-11-30 13:23:41 +0700

    setup fronten

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -6,7 +6,11 @@ const config = {
   preprocess: vitePreprocess(),
   kit: {
     adapter: adapter({
-      runtime: 'nodejs20.x'
+      out: 'build',
+      env: {
+        host: 'HOST',
+        port: 'PORT'
+      }
     })
   }
 };
